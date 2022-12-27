fa1762144285ae340846677e4ea91f5093178930
Christerpher Hunter <djhunter67@gmail.com>
2022-12-27 11:17:56 -0500

    ensure the bandwidth is set to zero

--- src/test_suite/mgtron_tester.py
+++ src/test_suite/mgtron_tester.py
@@ -33,7 +33,7 @@ def random_bandwidth_test():
     return bandwidth_list
 
 
-def channel_select() -> int:
+def channel_select() -> list[int]:
     """Select channel one through eight"""
     channel: list = [x for x in range(1, 9)]
     return channel
@@ -55,9 +55,9 @@ def random_test_suite():
                     frequency=frequency, channel=channel)
                 mgtron.change_power(
                     power_level=power, channel=channel)
-                # mgtron.change_bandwidth(
-                # percentage=bandwidth, channel=channel)
-                # time.sleep(1)
+                mgtron.change_bandwidth(
+                    percentage=0, channel=channel)
+                time.sleep(0.1)
                 print(
                     f"Channel: {F.YELLOW}{channel}{R}, Frequency: {F.YELLOW}{frequency}{R}, Power: {F.CYAN}{power}{R}")
 
@@ -66,21 +66,27 @@ def main():
 
     print(f"{F.GREEN}Welcome to the MGTron Tester{R}")
 
-    # a list of frequencies to test
-    frequency_list = random_frequency_test()
-    print(f"\n{F.GREEN}Frequency List: {frequency_list}{R}")
-
-    # a list of powers to test
-    power_list = random_power_test()
-    print(f"\n{F.GREEN}Power List: {power_list}{R}")
-
-    # a list of bandwidths to test
-    bandwidth_list = random_bandwidth_test()
-    print(f"\n{F.GREEN}Bandwidth List: {bandwidth_list}{R}\n")
-
     begin_time = time.time()
-    # print random test suite
-    random_test_suite()
+
+    mgtron = Megatron()
+    frequency_list = random_frequency_test()
+    power_list = random_power_test()
+    # bandwidth_list = random_bandwidth_test()
+    channel_list = channel_select()
+    for channel in channel_list:
+        for frequency in frequency_list:
+            for power in power_list:
+
+                mgtron.change_freq(
+                    frequency=frequency, channel=channel)
+                mgtron.change_power(
+                    power_level=power, channel=channel)
+                mgtron.change_bandwidth(
+                    percentage=0, channel=channel)
+                time.sleep(0.1)
+                print(
+                    f"Channel: {F.YELLOW}{channel}{R}, Frequency: {F.YELLOW}{frequency}{R}, Power: {F.CYAN}{power}{R}")
+
     end_time = time.time()
     print(f"\n{F.RED}Time Elapsed: {(end_time - begin_time) / 60:.2f}{R}")
 
